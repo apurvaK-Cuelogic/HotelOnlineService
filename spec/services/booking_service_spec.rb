5624928008d4f7f718849e957402f51593e2ac44
apurvaK-Cuelogic <apurva.kirti@cuelogic.com>
2018-02-06 20:07:55 +0530

    front page updated

--- spec/services/booking_service_spec.rb
+++ spec/services/booking_service_spec.rb
@@ -1,5 +1,23 @@
 require 'rails-helper'
 
 RSpec.describe BookingService do
-	context ''
+	context 'when the user is logged in,' do
+		context ' display the selected room details' do
+			context 'change room booking' do
+				it 'displays to the hotel details page'
+			end
+			context 'select a valid payment mode' do
+				conext 'when payment mode is valid' do
+					it 'should send a booking confirmation mail'
+				end
+				context 'when payment mode is invalid' do
+					it 'should notify the user'
+				end
+			end
+		end
+	end
+
+	context 'when the user is not logged in' do
+		it 'should notify the user to log in first'
+	end
 end
